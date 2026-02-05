4e81c6fc4b95d9fd4009d4099a48b160fe1b7d97
Unsalos <unsalos@outlook.com>
2026-02-05 16:39:02 -0400

    updated formatting

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -6,13 +6,14 @@ function App() {
     return(
     <div className="min-h-screen bg-sky-950 flex flex-col items-center p-15">
         <header className="text-center">
-        <h1 className="text-6xl font-bold tracking-tight text-amber-500">
-          LoopLM
-        </h1>
-        <p className="mt-3 text-amber-600">
-          Recursive Language Model
-        </p>
+            <h1 className="text-6xl font-bold tracking-tight text-amber-500">
+                LoopLM
+            </h1>
+            <p className="mt-3 text-amber-600">
+                Recursive Language Model
+            </p>
         </header>
+        
 
         <footer className="mt-auto pt-8 text-slate-300 text-sm">
             <a 
@@ -21,7 +22,7 @@ function App() {
             rel="noopener noreferrer"
             className="text-slate-300 hover:text-slate-500 font-medium transition-colors underline"
             >
-            ashtonlittle.ca
+                ashtonlittle.ca
             </a>
 
         </footer>
