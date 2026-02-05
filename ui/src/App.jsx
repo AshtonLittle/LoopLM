3d782f8c6c868570a6b5e4ff6d3316a7a701fdcf
Unsalos <unsalos@outlook.com>
2026-02-05 16:35:26 -0400

    changed "class" to "className"

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,17 +4,17 @@ import { useState } from 'react'
 function App() {
 
     return(
-    <div class="min-h-screen bg-sky-950 flex flex-col items-center p-15">
-        <header class="text-center">
-        <h1 class="text-6xl font-bold tracking-tight text-amber-500">
+    <div className="min-h-screen bg-sky-950 flex flex-col items-center p-15">
+        <header className="text-center">
+        <h1 className="text-6xl font-bold tracking-tight text-amber-500">
           LoopLM
         </h1>
-        <p class="mt-3 text-amber-600">
+        <p className="mt-3 text-amber-600">
           Recursive Language Model
         </p>
         </header>
 
-        <footer class="mt-auto pt-8 text-slate-300 text-sm">
+        <footer className="mt-auto pt-8 text-slate-300 text-sm">
             <a 
             href="https://ashtonlittle.ca" 
             target="_blank" 
